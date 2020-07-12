dda4dfe1d3f157d06f8c65bcd3bf85168b70b08c
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2020-07-12 06:35:44 -0300

    Inserindo fotos nivel normal e hard

--- scripts/index.js
+++ scripts/index.js
@@ -19,8 +19,11 @@
         main: document.getElementById('main'),
         board: document.getElementById('board'),
         easy: document.querySelector('#board #easy'),
+        easyCards: document.querySelectorAll('#board #easy .card'),
         normal: document.querySelector('#board #normal'),
+        normalCards: document.querySelectorAll('#board #normal .card'),
         hard: document.querySelector('#board #hard'),
+        hardCards: document.querySelectorAll('#board #hard .card'),
         leave: document.querySelector('#main h3')
     };
     let splashVisible = false;
@@ -38,7 +41,10 @@
 
     elementsHtml.levelEasy.addEventListener('click', playEasy);
 
-
+    elementsHtml.levelNormal.addEventListener('click', playNormal);
+    
+    elementsHtml.levelHard.addEventListener('click', playHard);
+    
     elementsHtml.leave.addEventListener('click', resetGame);
 
     // 	elementsHtml.facil.addEventListener('click', easy);
@@ -49,62 +55,61 @@
 
     // 	elementsHtml.msgs.addEventListener('click', hideMsgs);
 
-    function fetchImages(level) {
-        let path = `url(./images/${level}/)`;
+    async function fetchImages(level) {
+        let path = `url(./images/${level}/`;
         switch (level) {
-            case easy:
+            case 'easy':
+                for (let i = 0; i < 6; i++) {
+                    imagesCards[i] = `${path}${level}${i}.jpg)`;
+                };
+                break;
+            case 'normal':
                 for (let i = 0; i < 12; i++) {
-
-                }
+                    imagesCards[i] = `${path}${level}${i}.svg)`;
+                };
                 break;
-            case normal:
-                for (let i = 0; i < 24; i++) {
-
-                }
-                break;
-            case hard:
-                for (let i = 0; i < 36; i++) {
-
-                }
+            case 'hard':
+                for (let i = 0; i < 18; i++) {
+                    imagesCards[i] = `${path}${level}${i}.jpg)`;
+                };
                 break;
             default:
+                alert('Nível não encontrado');
                 break;
         }
     }
 
+    function returnDifferentsRandomNumbers(minNumber, maxNumber) {
+        var temp = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+        var numbers = [temp];
+        while (numbers.length < maxNumber) {
+            if (numbers.find(function (element) { return element === temp; }) === undefined) {
+                numbers.push(temp);
+            } else {
+                temp = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+            };
+        }
+        return numbers;
+    };
 
-
-    // 	function returnDifferentsRandomNumbers(minNumber, maxNumber) {
-    // 		var temp = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
-    // 		var numbers = [temp];
-    // 		while (numbers.length < maxNumber) {
-    // 			if (numbers.find(function (element) { return element === temp; }) === undefined) {
-    // 				numbers.push(temp);
-    // 			} else {
-    // 				temp = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
-    // 			};
-    // 		}
-    // 		return numbers;
-    // 	};
-
-    // 	async function populatingCards() {
-    // 		var temp = returnDifferentsRandomNumbers(0, elementsHtml.cards.length);
-    // 		for (var i = 0; i < elementsHtml.cards.length; i += 2) {
-    // 			elementsHtml.cards[temp[i]].style.backgroundImage = "url(\"./images/" + images[0] + "\")";
-    // 			elementsHtml.cards[temp[i]].style.backgroundSize = '0%';
-    // 			elementsHtml.cards[temp[i]].style.backgroundRepeat = 'no-repeat';
-    // 			elementsHtml.cards[temp[i]].style.backgroundPosition = 'center';
-    // 			elementsHtml.cards[temp[i + 1]].style.backgroundImage = "url(\"./images/" + images[0] + "\")";
-    // 			elementsHtml.cards[temp[i + 1]].style.backgroundSize = '0%';
-    // 			elementsHtml.cards[temp[i + 1]].style.backgroundRepeat = 'no-repeat';
-    // 			elementsHtml.cards[temp[i + 1]].style.backgroundPosition = 'center';
-    // 			images.shift();
-    // 			elementsHtml.cards[i].addEventListener('click', yourChance);
-    // 			elementsHtml.cards[i].setAttribute('id', "card-" + i);
-    // 			elementsHtml.cards[i + 1].addEventListener('click', yourChance);
-    // 			elementsHtml.cards[i + 1].setAttribute('id', "card-" + (i + 1));
-    // 		};
-    // 	};
+    async function populatingCards(levelCards) {
+        let levelLength = levelCards.length;
+        // console.log(levelLength);
+        let temp = returnDifferentsRandomNumbers(0, levelLength);
+        for (let i = 0; i < levelLength; i += 2) {
+            levelCards[temp[i]].style.backgroundImage = imagesCards[0];
+            levelCards[temp[i]].style.backgroundSize = '80%';
+            levelCards[temp[i]].style.backgroundRepeat = 'no-repeat';
+            levelCards[temp[i]].style.backgroundPosition = 'center';
+            levelCards[temp[i + 1]].style.backgroundImage = imagesCards[0];
+            levelCards[temp[i + 1]].style.backgroundSize = '80%';
+            levelCards[temp[i + 1]].style.backgroundRepeat = 'no-repeat';
+            levelCards[temp[i + 1]].style.backgroundPosition = 'center';
+            imagesCards.shift();
+            // levelCards[i].addEventListener('click', yourChance);
+            // levelCards[i + 1].addEventListener('click', yourChance);
+        };
+    };
 
     // 	function hideModal() {
     // 		elementsHtml.protection.style.width = '0%';
@@ -223,6 +228,10 @@
     function hideBoard(level){
         elementsHtml.leave.style.display = 'none';
         elementsHtml.board.style.display = 'none';
+        hideBoardMinimal(level);
+    }
+
+    function hideBoardMinimal(level){
         level.style.display = 'none';
     }
 
@@ -234,9 +243,9 @@
         pointsLevel.hard = 0;
         elementsHtml.leave.style.display = 'none';
         elementsHtml.board.style.display = 'none';
-        elementsHtml.easy.style.display = 'none';
-        elementsHtml.normal.style.display = 'none';
-        elementsHtml.hard.style.display = 'none';
+        hideBoardMinimal(elementsHtml.easy);
+        hideBoardMinimal(elementsHtml.normal);
+        hideBoardMinimal(elementsHtml.hard);
         cardsSelected = [undefined, undefined];
         imagesCards = [];
         showPanels(elementsHtml.options);
@@ -244,8 +253,36 @@
 
     function playEasy(){
         playingLevel = 'easy';
+        // console.log(playingLevel);
+        fetchImages(playingLevel);
+        // console.log(imagesCards);
+        populatingCards(elementsHtml.easyCards);
         hidePanels(elementsHtml.options);
         showBoard(elementsHtml.easy);
+    };
+    
+    function playNormal(){
+        playingLevel = 'normal';
+        // console.log(playingLevel);
+        fetchImages(playingLevel);
+        // console.log(imagesCards);
+        // console.log(elementsHtml.normalCards.length);
+        populatingCards(elementsHtml.normalCards);
+        hidePanels(elementsHtml.options);
+        showBoard(elementsHtml.normal);
+    };
+
+    function playHard() {
+        playingLevel = 'hard';
+        // console.log(playingLevel);
+        fetchImages(playingLevel);
+        // console.log(imagesCards);
+        // console.log(elementsHtml.hardCards.length);
+        elementsHtml.board.style.width = 'var(--widthBoardHard)';
+        elementsHtml.board.style.height = 'var(--heighBoardHard';
+        populatingCards(elementsHtml.hardCards);
+        hidePanels(elementsHtml.options);
+        showBoard(elementsHtml.hard);
     };
 
     function hideProtection() {
