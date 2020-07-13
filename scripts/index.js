3c3fae90a38c71466df8d3341e73ab1cf1b659de
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2020-07-13 15:09:04 -0300

    Ultimas antes de dar problema no descarregamento das imagens

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,7 @@
 
     elementsHtml.start.addEventListener('click', resetGame);
     
-    elementsHtml.restart.addEventListener('click', resetGame);
+    //elementsHtml.restart.addEventListener('click', resetGame);
 
     // 	elementsHtml.msgs.addEventListener('click', hideMsgs);
 
@@ -68,7 +68,7 @@
                 };
                 break;
             default:
-                alert('Nível não encontrado');
+                showMessage('Error in path images', 3000);
                 break;
         }
     }
@@ -99,6 +99,14 @@
         };
     };
 
+    async function cleaningCards(){
+        console.log(imagesCards);
+        for(let i = 0;i<(imagesCards.length * 2);i++){
+            imagesCards[i].style.backgroundImage = 'none';
+            imagesCards[i].removeEventListener('click', yourChance);
+        }
+    }
+
     function settingImageSize(){
         switch (playingLevel) {
             case 'easy':
@@ -111,7 +119,7 @@
                 return 'var(--sizeImagesHard)';
             break;        
             default:
-                showMessage('Error in image size',1000);
+                showMessage('Error in settingImageSize',1000);
             break;
         }
     }
@@ -153,7 +161,7 @@
             console.log('Cards Iguais');
             points++;
             if (points >= pointsLevel) {
-                theEnd();
+                showTheEnd();
             }
             removeClickCard();
             cardsSelected[0].style.cursor = 'none';
@@ -168,10 +176,6 @@
             hideProtection();
         }
     }
-
-    	function theEnd() {
-            showTheEnd(elementsHtml.theEnd);
-    	};
 
 
     // 	function reiniciarJogo() {
@@ -189,14 +193,14 @@
         }
     }
 
-    function showOptions(panel){
+    function showOptions(){
         elementsHtml.panels.style.display = 'flex';
-        panel.style.display = 'block';
+        elementsHtml.options.style.display = 'block';
     }
     
-    function showTheEnd(panel){
+    function showTheEnd(){
         elementsHtml.panels.style.display = 'flex';
-        panel.style.display = 'flex';
+        elementsHtml.theEnd.style.display = 'flex';
     }
 
     function hidePanels(panel){
@@ -211,30 +215,25 @@
 
     }
 
-    // function hideBoard(level){
-    //     elementsHtml.leave.style.display = 'none';
-    //     elementsHtml.board.style.display = 'none';
-    //     hideBoardMinimal(level);
-    // }
-
-    function hideBoardMinimal(level){
-        level.style.display = 'none';
+    function hideBoards(){
+        elementsHtml.leave.style.display = 'none';
+        elementsHtml.board.style.display = 'none';
+        elementsHtml.easy.style.display = 'none';
+        elementsHtml.normal.style.display = 'none';
+        elementsHtml.hard.style.display = 'none'; 
     }
 
-    function resetGame(){
-        document.location.reload();
-        // playingLevel = '';
-        // points = 0;
-        // pointsLevel = 0;
-        // elementsHtml.leave.style.display = 'none';
-        // elementsHtml.board.style.display = 'none';
-        // hideBoardMinimal(elementsHtml.easy);
-        // hideBoardMinimal(elementsHtml.normal);
-        // hideBoardMinimal(elementsHtml.hard);
-        // cardsSelected = [undefined, undefined];
-        // imagesCards = [];
-        // hidePanels(elementsHtml.theEnd);
-        // showOptions(elementsHtml.options);
+    async function resetGame(){
+        console.log(imagesCards);
+        await cleaningCards();
+        playingLevel = '';
+        points = 0;
+        pointsLevel = 0;
+        cardsSelected = [undefined, undefined];
+        imagesCards = [];
+        hidePanels(elementsHtml.theEnd);
+        hideBoards();
+        showOptions();
     }
 
     function playEasy(){
@@ -282,7 +281,7 @@
     function hideSplash() {
         elementsHtml.splash.style.display = 'none';
         splashVisible = false;
-        showOptions(elementsHtml.options);
+        showOptions();
         elementsHtml.header.style.display = 'block';
         elementsHtml.main.style.display = 'flex';
     };
